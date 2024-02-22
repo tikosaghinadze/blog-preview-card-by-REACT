94b05726a0c7b8a4974c7911517afb465d1c8c6a
tikosaghinadze <teasaghinadze20@gmail.com>
2024-02-22 20:14:15 +0400

    make one card component page using react,it was my first try by react and also add desktop designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+
+import Illustration from "./assets/images/illustration-article.svg";
+import Person from "./assets/images/image-avatar.webp";
 function App() {
-  return <>
-  
-  </>
+  return (
+    <>
+      <img
+        className="illustration-img"
+        src={Illustration}
+        alt="illustration article"
+      />
+      <button className="learning-btn">Learning</button>
+      <span className="published-data">Published 21 Dec 2023</span>
+      <h1>HTML & CSS foundations</h1>
+      <p className="description">
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+      <div className="person-info">
+        <img
+          style={{ width: "32px", height: "32px" }}
+          src={Person}
+          alt="person profile image"
+        />
+        <span style={{ fontSize: "14px", fontWeight: "800" }}>Greg Hooper</span>
+      </div>
+    </>
+  );
 }
 
 export default App;
